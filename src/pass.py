2a50dbac72c96cc7dddb683304e71accf923e87f
Flood <floodski@live.com>
2020-08-19 22:03:09 +1200

    Remembering syntax of python

--- src/pass.py
+++ src/pass.py
@@ -1,18 +1,21 @@
 import cherrypy
 import random
 
-class CoolPassGen:
+class CoolPassGen(object):
     def numberGen():
         return random.choice([69,420,666])
     
     def adjectiveGen():
-        return random.choice(["Shitting","Biting","Bending","Smirking"])
+        return random.choice(["Shitting","Biting","Bending","Smirking","Damp","Hungry"])
     
     def nounGen():
-        return random.choice(["Pope","Baby","Lover","Fiend","Friend"])
+        return random.choice(["Pope","Baby","Lover","Micky","Friend", "Cougar"])
     
-    def passGen():
-        return CoolPassGen.adjectiveGen() + CoolPassGen.nounGen() + str(CoolPassGen.numberGen())
+    def passGen(self):
+        return self.adjectiveGen() + self.nounGen() + str(self.numberGen())
 
-class main:
-    print(CoolPassGen.passGen())
+    @cherrypy.expose
+    def index(self):
+        return self.passGen()
+
+print(CoolPassGen.passGen(CoolPassGen))
