5644b2db1a02ebc42c52b1f7184f2c2aec01f2c0
Flood <floodski@live.com>
2020-08-19 21:47:40 +1200

    Added stubs for adjective, noun and number generators

--- src/pass.py
+++ src/pass.py
@@ -1,12 +1,23 @@
 import cherrypy
+import random
 
-
-
-class numberGen:
-    return random()
+class CoolPassGen:
+    def numberGen():
+        return random.choice([69,420,666])
+    
+    def adjectiveGen():
+        return "Shitting"
+    
+    def nounGen():
+        return "Pope"
+    
+    def passGen():
+        return adjectiveGen() + nounGen() + numberGen()
 
 class main:
     pre = "key"
     post = "john"
+    
+    print(CoolPassGen.numberGen())
     
     print(pre + post)
