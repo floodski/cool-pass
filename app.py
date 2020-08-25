5cccf8b5027dbf5a64b69e3c07956c30e4f16be4
floodski <floodski@live.com>
2020-08-25 23:47:21 +1200

    Clean up old code

--- app.py
+++ app.py
@@ -26,17 +26,5 @@ class CoolPassGen(object):
     def index():
         return CoolPassGen.passGen()
 
-    
-'''def runMainApp():
-    # Create an instance of MainApp and tell Cherrypy to send all requests under / to it. (ie all of them)
 
-    print ("========================================")
-    print ("Helios Server")
-    print ("========================================")
-    print ("Initialising...")
-
-if __name__ == '__main__':
-    
-#Run the function to start everything
-    runMainApp()'''
 	
