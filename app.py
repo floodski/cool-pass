c3308c66517a390f9583dc703f034b63f01593f0
James Flood <jflo015@aucklanduni.ac.nz>
2020-08-25 20:37:10 +1200

    Now pushing the previous app but now w it uses flask

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
+#!usr/bin/python3
+import sys
 from flask import Flask
+import random
+import sqlite3
+
+import cherrypy
+import random
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello World"
+class CoolPassGen(object):
+    def numberGen():
+        return random.choice([69,420,666])
+    
+    def adjectiveGen():
+        return random.choice(["Shitting","Biting","Bending","Smirking","Damp","Hungry"])
+    
+    def nounGen():
+        return random.choice(["Pope","Baby","Lover","Micky","Friend", "Cougar"])
+    
+    def passGen():
+        return self.adjectiveGen() + self.nounGen() + str(self.numberGen())
+
+    @app.route("/")
+    def index():
+        return CoolPassGen.passGen(CoolPassGen)
+
+    
+'''def runMainApp():
+    # Create an instance of MainApp and tell Cherrypy to send all requests under / to it. (ie all of them)
+
+    print ("========================================")
+    print ("Helios Server")
+    print ("========================================")
+    print ("Initialising...")
+
+if __name__ == '__main__':
+    
+#Run the function to start everything
+    runMainApp()'''
+	
