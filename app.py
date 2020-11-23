ca368a1cf58e93c05c5f4212b64e394d45557d8b
Flood <floodski@live.com>
2020-11-23 22:46:17 +1300

    Added a normal mode and now a dipshit mode

--- app.py
+++ app.py
@@ -10,22 +10,42 @@ import random
 app = Flask(__name__)
 
 class CoolPassGen(object):
+    
+    dipshitMode = False
+    
     def numberGen():
-        return random.choice([69,420,666])
+        if(CoolPassGen.dipshitMode):
+            return random.choice([69,420,666])
+        
+        return random.choice(["777","22"])
     
     def adjectiveGen():
-        return random.choice(["Shitting","Biting","Bending","Smirking","Damp","Hungry"])
+        if(CoolPassGen.dipshitMode):
+            return random.choice(["Shitting","Biting","Bending","Smirking","Damp","Hungry"])
+        
+        return random.choice(["Lovely","Kind"])
     
     def nounGen():
-        return random.choice(["Pope","Baby","Lover","Micky","Friend", "Cougar"])
+        if(CoolPassGen.dipshitMode):
+            return random.choice(["Pope","Baby","Lover","Micky","Friend", "Cougar"])
+        
+        return random.choice(["Ducky","Puppy"])
     
     def passGen():
-        return CoolPassGen.adjectiveGen() + CoolPassGen.nounGen() + str(CoolPassGen.numberGen())
+        return str(CoolPassGen.adjectiveGen()) + str(CoolPassGen.nounGen()) + str(CoolPassGen.numberGen())
 
     @app.route("/")
-    def index():
+    def index(string):
+        dipshitMode = False
         return CoolPassGen.passGen()
+        #print(CoolPassGen.passGen())
     
     @app.route("/stupid")
-    def index2():
+    def index2(string):
+        dipshitMode = True
         return CoolPassGen.passGen()
+        #print(CoolPassGen.passGen())
+
+'''    
+CoolPassGen.index("ok")
+CoolPassGen.index2("dumb")'''
